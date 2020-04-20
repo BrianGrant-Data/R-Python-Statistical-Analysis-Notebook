16986d1f53322818ca1288cc74673aa1840330ee
Bagmon <gra11027@byui.edu>
2020-04-20 17:57:53 +0000

    I updated the preparatory script.

---
+++ 0 Preparatory Scripts/R Packages Install.R
@@ -0,0 +1,6 @@
+# R Packages to install before working
+install.packages(c("mosaic", "plotly", "tidyverse", "dplyr"))
+
+# R Packages of data sets
+install.packages(c("nycflights13", "ggplot2movies"))
+
