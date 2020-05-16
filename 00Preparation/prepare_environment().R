db3a63e46edf2ceeef6054240b21cbaa80ce89eb
Bagmon <gra11027@byui.edu>
2020-05-16 01:42:07 -0600

    added the datasets, carData, ggplot2, and car packages to my general list of packages to run before working.

--- 00Preparation/prepare_environment().R
+++ 00Preparation/prepare_environment().R
@@ -2,13 +2,18 @@
 ## install.packages(c("dplyr", "ggplot2movies", "mosaicCore", "pander", "shiny", "tidyverse"))
 install.packages("shiny")
 install.packages("mosaic")
+install.packages("datasets")
+install.packages("carData")
 
 library("ggplot2movies")
+library("ggplot2")
 library("shiny")
 library("mosaicCore")
 library("pander")
 library("tidyverse")
 library("dplyr")
+library("car")
+
 
 #?lapply
 #see https://stackoverflow.com/questions/8175912/load-multiple-packages-at-once
